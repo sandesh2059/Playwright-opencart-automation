bd84a74a274a4df99e7d5f3b753756f174d57208
Sandesh <sandeshchy2059@gmail.com>
2026-08-14 11:49:41 +0545

    added test of  product search with complete name

--- tests/Search_test.spec.js
+++ tests/Search_test.spec.js
@@ -13,6 +13,7 @@ test('search with exact product name', async ({ page }) => {
   await page.getByRole('link', { name: 'Your Store' }).click();
   await page.getByRole('textbox', { name: 'Search' }).click();
   await page.getByRole('textbox', { name: 'Search' }).fill('iphone');
+  await page.locator('button[type="submit"].btn-light.btn-lg').click();
   await expect(page.getByRole('link', { name: 'iPhone' }).last()).toBeVisible();
     
 });
@@ -30,5 +31,26 @@ test('search with partial product name', async ({ page }) => {
   await page.getByRole('link', { name: 'Your Store' }).click();
   await page.getByRole('textbox', { name: 'Search' }).click();
   await page.getByRole('textbox', { name: 'Search' }).fill('iph');
+  await page.locator('button[type="submit"].btn-light.btn-lg').click();
   await expect(page.getByRole('link', { name: 'iPhone' }).last()).toBeVisible();
 });
+
+test('search with complete product name', async ({ page }) => {
+    await page.goto('http://localhost/index.php');
+    await page.getByRole('link', { name: ' My Account ' }).click();
+    await page.getByRole('link', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'E-Mail Address' }).click();
+    await page.getByRole('textbox', { name: 'E-Mail Address' }).fill('Sandeshchy2059@gmail.com');
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill('Sandesh@12');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL(/route=account\/account/);
+    await page.getByRole('link', { name: 'Your Store' }).click();
+    await page.getByRole('textbox', { name: 'Search' }).click();
+    await page.getByRole('textbox', { name: 'Search' }).fill('Samsung SyncMaster 941BW');
+    
+
+    await page.locator('button[type="submit"].btn-light.btn-lg').click();
+
+    await expect(page.getByRole('link', { name: 'Samsung SyncMaster 941BW' }).last()).toBeVisible();
+})
