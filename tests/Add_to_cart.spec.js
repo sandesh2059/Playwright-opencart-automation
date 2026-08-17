64b7ab2ef65f5e05ce16f5ba0e20b986f171a594
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 16:25:26 +0545

    add a product to the cart with quantity = 1.5

--- tests/Add_to_cart.spec.js
+++ tests/Add_to_cart.spec.js
@@ -49,7 +49,7 @@ test('Add a product with quantity = 0 to the cart', async ({ page }) => {
   await page.locator('#input-quantity').click();
   await page.locator('#input-quantity').fill('0');
   await page.getByRole('button', { name: 'Add to Cart' }).click();
-  await expect(page.getByText('Success: You have added')).toBeVisible(); // This assertion checks for the success message, but it doesn't verify the quantity in the cart. it should be updated to check the quantity in the cart to ensure that adding a product with quantity 0 does not actually add it to the cart.
+  await expect(page.getByText('Warning: Quantity should only accept positive whole numbers')).toBeVisible(); // Message displayed was : 'Success: You have added'
 });
 
 test('Rapidly click the "Add to Cart" button multiple times', async ({ page }) => {
@@ -103,4 +103,26 @@ test('Add a product to the cart with quantity = ajdhf@', async ({ page }) => {
   await page.locator('#input-quantity').fill('ajdhf@');
   await page.getByRole('button', { name: 'Add to Cart' }).click();
   await expect(page.getByText('Warning: Quantity should only accept positive whole numbers')).toBeVisible(); // Message displayed was : 'Success: You have added'
+
+});
+
+test('Add a product to the cart with quantity = 1.5', async ({ page }) => {
+  await page.goto('http://localhost/index.php');
+  await page.getByRole('link', { name: ' My Account ' }).click();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'E-Mail Address' }).click();
+  await page.getByRole('textbox', { name: 'E-Mail Address' }).fill('sandeshchy2059@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Sandesh@12');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL(/route=account\/account/);
+  await page.goto('http://localhost/index.php');
+  await page.locator('#content').getByText('MacBook').click();
+  await page.locator('#input-quantity').click();
+  await page.locator('#input-quantity').fill('1.5');
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await expect(page.getByText('Success: You have added')).toBeVisible();
+  await page.getByRole('link', { name: ' Shopping Cart' }).click();
+  await expect(page.getByRole('heading', { name: 'Shopping Cart' })).toBeVisible();
+  await expect(page.locator('#output-cart').getByText('MacBook')).toBeVisible();
 });
