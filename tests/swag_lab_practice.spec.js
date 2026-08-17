f1813f068bc87d392e960b66249a843b5042adcf
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 15:59:51 +0545

    verify locked out user cannot login

--- tests/swag_lab_practice.spec.js
+++ tests/swag_lab_practice.spec.js
@@ -114,6 +114,16 @@ test('Add single item to cart', async ({ page }) => {
   await expect(page.locator('[data-test="item-1-title-link"]')).toBeVisible();
 });
 
+test('verify locked out user cannot login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('locked_out_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
 test('Add multiple items to cart', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
