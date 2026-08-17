8c093ae94793e63fb9eedc6580be35a174b4de15
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 15:27:02 +0545

    login validation for username

--- tests/swag_lab_practice.spec.js
+++ tests/swag_lab_practice.spec.js
@@ -32,7 +32,21 @@ test('Login with invalid credentials', async ({ page }) => {
   await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
 
+test('Login with empty credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
 
+test('verify if username is case sensitive', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('STANDARD_USER');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
 
 test('Add single item to cart', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
