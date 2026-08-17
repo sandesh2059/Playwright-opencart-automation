93231a1f3c651a592b7e7e14fa151c9a428668b4
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 15:24:55 +0545

    repidly click add to cart multiple times

--- tests/Add_to_cart.spec.js
+++ tests/Add_to_cart.spec.js
@@ -51,3 +51,41 @@ test('Add a product with quantity = 0 to the cart', async ({ page }) => {
   await page.getByRole('button', { name: 'Add to Cart' }).click();
   await expect(page.getByText('Success: You have added')).toBeVisible(); // This assertion checks for the success message, but it doesn't verify the quantity in the cart. it should be updated to check the quantity in the cart to ensure that adding a product with quantity 0 does not actually add it to the cart.
 });
+
+test('Rapidly click the "Add to Cart" button multiple times', async ({ page }) => {
+  await page.goto('http://localhost/index.php');
+  await page.getByRole('link', { name: ' My Account ' }).click();
+  await page.getByRole('link', { name: 'Login' }).click();
+  await page.getByRole('textbox', { name: 'E-Mail Address' }).click();
+  await page.getByRole('textbox', { name: 'E-Mail Address' }).fill('sandeshchy2059@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Sandesh@12');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL(/route=account\/account/);
+  await page.goto('http://localhost/index.php');
+
+  await page.getByRole('textbox', { name: 'Search' }).click();
+  await page.getByRole('textbox', { name: 'Search' }).fill('samsung');
+  await page.locator('button[type="submit"].btn-light.btn-lg').click();
+
+  await page.locator('#product-list').getByText('Samsung SyncMaster 941BW').click();
+
+  // Now there's only one "Add to Cart" button on the page
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+  await page.getByRole('button', { name: 'Add to Cart' }).click();
+
+
+  await expect(page.getByText('Success: You have added').last()).toBeVisible();
+  await page.getByRole('link', { name: ' Shopping Cart' }).click();
+  await expect(page.getByRole('heading', { name: 'Shopping Cart' })).toBeVisible();
+  await expect(page.locator('#output-cart').getByText('Samsung SyncMaster 941BW')).toBeVisible();
+
+
+
+});
