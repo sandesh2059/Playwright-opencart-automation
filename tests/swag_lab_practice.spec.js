cee885b631433e484254345bddd038d1476a0869
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 13:51:39 +0545

    verify product detail for swag_lab

--- tests/swag_lab_practice.spec.js
+++ tests/swag_lab_practice.spec.js
@@ -114,3 +114,14 @@ test('Logout from the application', async ({ page }) => {
 });
 
 
+test('Verify product details', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await expect(page.locator('[data-test="inventory-item-name"]')).toBeVisible();
+});
