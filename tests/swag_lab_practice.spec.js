fc4281d4269348bc67e362c21009c898afffd0e6
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 15:31:55 +0545

    verify if password is case sensitive

--- tests/swag_lab_practice.spec.js
+++ tests/swag_lab_practice.spec.js
@@ -48,6 +48,16 @@ test('verify if username is case sensitive', async ({ page }) => {
   await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
 
+test('verify if password is case sensitive', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('SECRET_SAUCE');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
 test('Add single item to cart', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
