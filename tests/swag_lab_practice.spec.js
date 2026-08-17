2d2db9a114b3b14ae0afde09b92c2551ed20c8f3
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 12:27:10 +0545

    add single item to cart for swag_lab

---
+++ tests/swag_lab_practice.spec.js
@@ -0,0 +1,20 @@
+/* this is a file created during practice of playwright automation,
+    it is not part of the project, so please ignore this file,
+    it is just for practice purpose,
+    i visited https://www.saucedemo.com/ website to practice automation testing */
+
+
+
+import { test, expect } from '@playwright/test';
+
+test('Add single item to cart', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await expect(page.locator('[data-test="item-1-title-link"]')).toBeVisible();
+});
