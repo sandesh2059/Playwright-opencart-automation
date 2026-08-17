69660498c95818dc3b8a1011af3b0b2c6b474c32
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 13:31:04 +0545

    Login with invalid credentials for swag_lab

--- tests/swag_lab_practice.spec.js
+++ tests/swag_lab_practice.spec.js
@@ -17,6 +17,16 @@ test('Login with valid credentials', async ({ page }) => {
   await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
+test('Login with invalid credentials', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('invalid_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('invalid_password');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
+
 test('Add single item to cart', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
