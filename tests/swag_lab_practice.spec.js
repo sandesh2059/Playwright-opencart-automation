0f13d4ef14d52f1f1a728424afdfb09e4182da20
Sandesh <sandeshchy2059@gmail.com>
2026-08-17 12:32:25 +0545

    add multiple items to cart for swag_lab

--- tests/swag_lab_practice.spec.js
+++ tests/swag_lab_practice.spec.js
@@ -18,3 +18,23 @@ test('Add single item to cart', async ({ page }) => {
   await page.locator('[data-test="shopping-cart-link"]').click();
   await expect(page.locator('[data-test="item-1-title-link"]')).toBeVisible();
 });
+
+test('Add multiple items to cart', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-fleece-jacket"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await expect(page.locator('[data-test="item-0-title-link"]')).toBeVisible();
+  await expect(page.locator('[data-test="item-1-title-link"]')).toBeVisible();
+  await expect(page.locator('[data-test="item-4-title-link"]')).toBeVisible();
+  await expect(page.locator('[data-test="item-5-title-link"]')).toBeVisible();
+
+
+});
